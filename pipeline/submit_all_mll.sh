324d3bb85b744f06551b6c2505419549df594556
josegcpa <josegcpa@ebi.ac.uk>
2021-09-17 18:14:23 +0100

    added scripts to extract examples of wbc and rbc from slides

--- pipeline/submit_all_mll.sh
+++ pipeline/submit_all_mll.sh
@@ -7,14 +7,14 @@ do
     slide_name=$(basename $file)
     log_file=logs/salim_$slide_name.o
     log_file_err=logs/salim_$slide_name.e
-    #bsub\
-    #    -P gpu\
-    #    -M 16000\
-    #    -gpu "num=1:j_exclusive=yes"\
-    #    -g /salim_gpu\
-    #    -J SALIM_$(basename $file | cut -d '.' -f 1)\
-    #    -o logs/salim_$(basename $slide_name).o\
-    #    -e logs/salim_$(basename $slide_name).e\
-    #    -W 24:00\
+    bsub\
+        -P gpu\
+        -M 16000\
+        -gpu "num=1:j_exclusive=yes"\
+        -g /salim_gpu\
+        -J SALIM_$(basename $file | cut -d '.' -f 1)\
+        -o logs/salim_$(basename $slide_name).o\
+        -e logs/salim_$(basename $slide_name).e\
+        -W 24:00\
     sh pipeline.sh $file $out_path $UNET_CKPT $QC_CKPT
 done
