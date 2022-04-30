3d5fdd0929020e0b0af2d3459807d144613a9d92
josegcpa <josegcpa@ebi.ac.uk>
2022-04-30 13:52:51 +0100

    added script for downloads

---
+++ download-and-unzip-data.sh
@@ -0,0 +1,17 @@
+# data for Morphotype analysis
+## download 
+wget -O morphotype-input.zip https://figshare.com/ndownloader/files/34404533
+## unzip
+unzip morphotype-input.zip -d mil-comori
+
+# output for Morphotype analysis
+## download
+wget -O morphotype-output.zip https://figshare.com/ndownloader/files/34402946
+## unzip
+unzip morphotype-output.zip -d mil-comori
+
+# data for analysis-plotting
+## download
+wget -O analysis-plotting.zip https://figshare.com/ndownloader/files/34642040
+## unzip
+unzip analysis-plotting.zip -d analysis-plotting
